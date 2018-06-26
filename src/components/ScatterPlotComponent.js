0d08585114e52aa9af66feba7eb8019e8218a3c3
Arumugavelan S <s.arumugavelan@gmail.com>
2018-06-26 21:14:12 +0530

    Calculated height and width of canvas

--- src/components/ScatterPlotComponent.js
+++ src/components/ScatterPlotComponent.js
@@ -8,7 +8,6 @@ class Scatterplotcomponent extends Component{
 
 	constructor(props, context){
 		super(props,context);
-
 		this.options = {};
         this.options.data = plotterpoints;
 
@@ -16,8 +15,8 @@ class Scatterplotcomponent extends Component{
 	}
 
 	
-drawPlots(ctx, upperLeftCornerX, upperLeftCornerY,color){
-	console.log("drawPlots executed ---------> ",ctx, upperLeftCornerX, upperLeftCornerY)
+    drawPlots(ctx, upperLeftCornerX, upperLeftCornerY,color){
+       console.log("drawPlots executed ---------> ",ctx, upperLeftCornerX, upperLeftCornerY)
     //ctx.save();
     ctx.fillStyle=color;
     ctx.beginPath();
@@ -27,32 +26,64 @@ drawPlots(ctx, upperLeftCornerX, upperLeftCornerY,color){
     ctx.restore();
 }
 
- drawLine(ctx, startX, startY, endX, endY,color){
- 	console.log("drawLine------",ctx, startX, startY, endX, endY,color)
-    ctx.save();
-    ctx.strokeStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(startX,startY);
-    ctx.lineTo(endX,endY);
-    ctx.stroke();
-    ctx.restore();
+drawLine(ctx, startX, startY, endX, endY,color){
+  console.log("drawLine------",ctx, startX, startY, endX, endY,color)
+  ctx.save();
+  ctx.strokeStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(startX,startY);
+  ctx.lineTo(endX,endY);
+  ctx.stroke();
+  ctx.restore();
+}
+
+getCanvasWidth(){
+    let maxWidth = plotterpoints[0].start_time;
+    for(let i=1; i<plotterpoints.length;i++){
+        if(maxWidth<plotterpoints[i].start_time){
+            maxWidth = plotterpoints[i].start_time;
+        }
+
+    }
+
+    if((maxWidth%100)!==0){
+        maxWidth = maxWidth + (maxWidth%100)
+    }
+    return maxWidth;
+}
+
+getCanvasHeight(){
+    let maxHeight = plotterpoints[0].duration;
+    for(let i=1; i<plotterpoints.length;i++){
+        if(maxHeight<plotterpoints[i].duration){
+            maxHeight = plotterpoints[i].duration;
+        }
+
+    }
+
+    if((maxHeight%100)!==0){
+        maxHeight = maxHeight + (maxHeight%100)
+    }
+    return maxHeight;
 }
 
 scatterPlotChart(){
 	const canvas = this.refs.canvas;
-	let ctx = canvas.getContext("2d");
-	this.options.gridScale = 5;
-	 var maxValue = 10;
-        var canvasActualHeight = canvas.height;
-        var canvasActualWidth = canvas.width;
-        console.log("maxValue  ",maxValue)
- 
+    canvas.width = this.getCanvasWidth();
+    canvas.height = this.getCanvasHeight();
+    let ctx = canvas.getContext("2d");
+    this.options.gridScale = 5;
+    var maxValue = 10;
+    var canvasActualHeight = canvas.height;
+    var canvasActualWidth = canvas.width;
+    console.log("maxValue  ",maxValue)
+    
         //drawing the grid lines
         let gridY = 0;
 
-        for(let i=0; i< 10; i++){
-gridY = canvas.height/10 + gridY;
-let gridValue = 0;
+        for(let i=0; i< canvas.height/10; i++){
+            gridY = canvas.height/10 + gridY;
+            let gridValue = 0;
 
             this.drawLine(
                 ctx,
@@ -61,18 +92,18 @@ let gridValue = 0;
                 canvas.width,
                 gridY,
                 '#eeeee'
-            );
-             
+                );
+            
             //writing grid markers
             ctx.save();
             ctx.fillStyle = '#eeeee';
             ctx.font = "bold 10px Arial";
             if(i%2===0){
-           ctx.fillText(5 - (i/2) + ' min', 10,gridY - 2);
-       }
-            ctx.restore();
-            gridValue+=2;
-        }
+             ctx.fillText(5 - (i/2) + ' min', 10,gridY - 2);
+         }
+         ctx.restore();
+         gridValue+=2;
+     }
 
 
 
@@ -85,7 +116,7 @@ let gridValue = 0;
 
         var totalPlots = this.options.data;
         
- let color = 'grey';
+        let color = 'grey';
         for (let i=0; i<totalPlots.length; i++){
         	console.log("totalPlots------------- ",i)
         	if(totalPlots[i].status==='error'){
@@ -100,39 +131,32 @@ let gridValue = 0;
                 totalPlots[i].start_time,
                 totalPlots[i].duration,
                 color
-            );
+                );
         }
-}
+    }
+
+    
 
 
-
-
-
- 
-
-
-	componentDidMount() {
-		const canvas = this.refs.canvas;
-		const ctx = canvas.getContext("2d");
+    componentDidMount() {
+      const canvas = this.refs.canvas;
+      const ctx = canvas.getContext("2d");
 		 // this.drawLine(ctx,0,0,100,100,'red');
-		this.scatterPlotChart();
-		
-  }
+       this.scatterPlotChart();
+       
+   }
 
-  
-
-	render(){
-		console.log("props", this.props)
-
-		return(
-	<div>	
-    <canvas ref="canvas" width={this.props.width} height={this.props.height} />
-    </div>
    
 
+   render(){
+      console.log("props", this.props)
 
-			)
-	}
+      return(
+       <div>	
+       <canvas ref="canvas" />
+       </div>
+       )
+   }
 }
 
 export default Scatterplotcomponent;
